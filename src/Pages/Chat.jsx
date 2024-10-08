7765d4c2dfaae09e1d4e45866136e74d531fa6b8
runquan-ray-zhou <151119924+runquan-ray-zhou@users.noreply.github.com>
2024-10-07 20:45:19 -0400

    change api address

--- src/Pages/Chat.jsx
+++ src/Pages/Chat.jsx
@@ -117,7 +117,7 @@ export default function Chat() {
     const finalTargetLanguage = targetLanguage || "en-US";
 
     try {
-      const response = await fetch("http://localhost:3123/api/location", {
+      const response = await fetch("https://pat-io.onrender.com/api/location", {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
@@ -164,7 +164,7 @@ export default function Chat() {
     const finalTargetLanguage = targetLanguage || "en-US";
 
     try {
-      const response = await fetch("http://localhost:3123/api/chat", {
+      const response = await fetch("https://pat-io.onrender.com/api/chat", {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
@@ -411,7 +411,7 @@ export default function Chat() {
         userInteractions: updatedUserInteractions, // This should contain the correct data
       });
 
-      const response = await fetch("http://localhost:3123/api/chat", {
+      const response = await fetch("https://pat-io.onrender.com/api/chat", {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
