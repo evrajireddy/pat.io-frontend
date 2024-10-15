31c37e37f63942892dad79691cf7264366c7a23b
runquan-ray-zhou <151119924+runquan-ray-zhou@users.noreply.github.com>
2024-10-14 22:27:49 -0400

    add tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,7 @@
 module.exports = {
-  content: [
-    "./src/**/*.{js,jsx,ts,tsx}",
-  ],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {},
   },
   plugins: [],
-  
-  darkMode: 'class',
-}
+};
