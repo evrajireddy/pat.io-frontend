d51129b1f3f6ef6560fb997b2d95901024610844
runquan-ray-zhou <151119924+runquan-ray-zhou@users.noreply.github.com>
2024-10-18 02:00:21 -0400

    update breadcrumb

--- src/Componets/BreadCrumb.jsx
+++ src/Componets/BreadCrumb.jsx
@@ -2,13 +2,13 @@ import React from "react";
 
 export default function BreadCrumb({ path, onNavigate }) {
   return (
-    <div className="bg-blue-500 hover:bg-gray-700 text-white font-semibold py-1 px-2 rounded mb-4 mt-5 mr-5 w-auto">
+    <div>
       {path.map((item, index) => (
         <span key={index}>
           {index > 0 && " > "}
           <button
             onClick={() => onNavigate(index, item)}
-            className="breadcrumb-button"
+            className="breadcrumb-button hover:bg-gray-200 text-b font-semibold py-1 px-2 rounded mb-4 mt-5 mr-5 w-auto"
           >
             {item}
           </button>
